250026bc68de00c9421611d47105cab2fb658857
art group of companies <artleasing2025@gmail.com>
2025-11-08 00:10:38 +0530

    db

--- assets/js/database.js
+++ assets/js/database.js
@@ -4,25 +4,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebas
 import { getFirestore,  getDoc,  doc,  addDoc,  updateDoc, deleteDoc,  collection,  getDocs,  query,  where, orderBy,  serverTimestamp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-//---------------main db link----------------------------------
- /*  apiKey: "AIzaSyCOcsL9ahdFUunWDAcv5JZdnMxMZehquhg",
+
+  apiKey: "AIzaSyCOcsL9ahdFUunWDAcv5JZdnMxMZehquhg",
     authDomain: "art-staffmanage.firebaseapp.com",
     projectId: "art-staffmanage",
     storageBucket: "art-staffmanage.firebasestorage.app",
     messagingSenderId: "951269778971",
     appId: "1:951269778971:web:11fbb26385588ced133abe"  
-    */
-//---------------------------------------------------------------
-
-  
-//------------test db--------------------------------------------
-  apiKey: "AIzaSyArh9Lqdyio6QZ276L2ciMwEgU8BgigrFs",
-    authDomain: "user-atten.firebaseapp.com",
-    projectId: "user-atten",
-    storageBucket: "user-atten.firebasestorage.app",
-    messagingSenderId: "13092297593",
-    appId: "1:13092297593:web:dd99a876e5dec1714fada8"   
-//---------------------------------------------------------------------
+    
 
 };
 
